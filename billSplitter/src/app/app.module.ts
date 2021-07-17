a1584949599dc42feaeebf612228171e2613d9d9
server007 <sarvagyasingh7@gmail.com>
2021-07-17 15:03:08 +0530

    initial commit

---
+++ billSplitter/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+
+import { NgxPaginationModule } from 'ngx-pagination';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
